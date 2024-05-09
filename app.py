090d4e8ee1c5412dbb009c5b6067af8567c4e373
Kelsey Brahe Grover <kgrover3@myune.edu.au>
2024-05-09 13:00:45 -0600

    Added Kelsey's changes

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 from flask import render_template
-# from PIL import Image
 from pathlib import Path
-from werkzeug.utils import send_from_directory, secure_filename
-# import cv2
-# import tkinter as tk
-# from tkinter import filedialog
-# import numpy as np
+from werkzeug.utils import secure_filename
 import os
-from ultralytics import YOLO
-from classifer import get_img_classification, check_whether_target_directory_exists
-# from keras import load_model
+import shutil
+from classifer import get_img_classification
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = 'uploads'
+RESULTS_FOLDER = 'results'
 IMG_COUNTER = 0
 SUBFOLDER = ''
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-IMAGES = os.listdir(os.path.join(app.static_folder, "results"))
+app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
 
-# load the model
-# model = load_model("D:/PATH/TO/MODEL")
+def ensure_folders_exist():
+    # Create uploads folder if it doesn't exist
+    uploads_folder = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(uploads_folder):
+        os.makedirs(uploads_folder)
+        print(f"Created '{uploads_folder}' folder.")
+
+    # Create results folder if it doesn't exist
+    results_folder = os.path.join(app.static_folder, app.config['RESULTS_FOLDER'])
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+        print(f"Created '{results_folder}' folder.")
+
+ensure_folders_exist()
+
+IMAGES = os.listdir(os.path.join(app.static_folder, "results"))
 
 @app.route("/")
 def hello_world():
@@ -34,6 +43,7 @@ def hello_world():
 # will save to the /uploads folder.
 # From here we can loop through the images to process them.
 # We will need to clean the uploads folder after processing the images.
+#
 @app.route('/upload', methods=["POST"])
 def uploadFiles():
     if request.method == "POST":
@@ -45,10 +55,10 @@ def uploadFiles():
                     filename = secure_filename(f.filename)
                     filepath = os.path.join(basepath, 'uploads', filename)
                     f.save(filepath)
-                return render_template("index.html")
+                return render_template("index.html", ready=True)
             except FileNotFoundError:
                 print('File not found.')
-                return render_template("index.html")
+                return render_template("index.html", empty=True)
 
 #
 # This function will get the value of the bounding box and call
@@ -56,27 +66,63 @@ def uploadFiles():
 # If the bounding box is selected 'boundingBox' will equal "on", else None
 # It will need to process all of the images in the uploads folder,
 # save them to the /results folder
+#
 @app.route('/predict', methods=["POST"])     
 def predict():
     if request.method == "POST":
-        # this is the value of the bounding box
-        boundingBox = request.form.get("bBox")
-        print(boundingBox == "on")
-
         # run image prediction and save to results
-        print(check_whether_target_directory_exists(os.path.join(app.static_folder, "results")))
-        # get_img_classification("src/models/best.pt", "/uploads", os.path(app.static_folder), "/results")
+        source_directory = os.path.join(os.path.dirname(__file__), 'uploads')
+        if(len(os.listdir(source_directory)) == 0):
+            return render_template("index.html", empty=True)
+        else:
+            # Iterate through subdirectories and move files to uploads folder
+            for root, dirs, files in os.walk(source_directory):
+                for file in files:
+                    src_path = os.path.join(root, file)
+                    dst_path = os.path.join(source_directory, file)
+                    shutil.move(src_path, dst_path)
+            results = get_img_classification('src/models/best.pt', source_directory, app.static_folder, "results")
+            # get statistics
+            for result in results.get('boxes'):
+                animal = str(result.get('animal'))
+                confidence_score = str(result.get('confidence_score'))
+                box_coordinates_normalised = str(result.get('box_coordinates_normalised'))
+                image_filename = str(result.get('image_filename'))
+                # get path to correct subfolder
+                save_path = os.path.join(app.static_folder, "results" + "/" + animal)
+                # save to a txt file
+                filename = os.path.join(save_path, image_filename.split('.')[0] + ".txt")
+                file = open(filename, "w")
+                file.write(
+                    animal + "\n" +
+                    confidence_score + "\n" +
+                    box_coordinates_normalised + "\n" +
+                    image_filename + "\n"
+                )
+                file.close()
+            
+            # get list of all subdirectories in /results
+            subfolders = os.listdir(os.path.join(app.static_folder, "results"))
+            # clear the uploads folder
+            [os.remove(f) if f.is_file() else shutil.rmtree(f) 
+             for f in Path(os.path.join(os.path.dirname(__file__), 'uploads')).rglob("*") 
+             if f.is_file() or os.path.isdir(f)]
+            
+            return render_template("index.html", subfolders=subfolders, complete=True)
 
-        # get list of all subdirectories in /results
-        subfolders = os.listdir(os.path.join(app.static_folder, "results"))
-
-        # clear the uploads folder
-        [f.unlink() for f in Path(os.path.join(os.path.dirname(__file__), 'uploads')).glob("*") if f.is_file()]
-
+#
+# This image will display the subfolders created during image prediction
+#
+@app.route('/view_images', methods=["POST"])
+def view():
+    # Get list of all subdirectories in /results excluding .log files
+    subfolders = [folder for folder in os.listdir(os.path.join(app.static_folder, "results")) 
+                  if os.path.isdir(os.path.join(app.static_folder, "results", folder)) and not folder.endswith('.log')]
     return render_template("index.html", subfolders=subfolders)
 
-
+#
 # Display images in selected subfolder
+#
 @app.route('/display', methods=["POST"])
 def display():
     subfolder = request.form['subfolder']
@@ -88,16 +134,37 @@ def display():
     global SUBFOLDER
     SUBFOLDER = subfolder
     current_img = "/static/results/" + SUBFOLDER + "/" + IMAGES[IMG_COUNTER]
+    while(
+        not current_img.lower().endswith(('.png', '.jpg', '.jpeg', 'tiff', '.bmp', '.gif'))
+        and IMG_COUNTER != len(IMAGES) - 1
+    ):
+        IMG_COUNTER += 1
+        current_img = "/static/results/" + SUBFOLDER + "/" + IMAGES[IMG_COUNTER]
+    
+    stats = display_stats(IMAGES[IMG_COUNTER].split('.')[0], SUBFOLDER)
+    if stats is not None:
+        return render_template("index.html", 
+                            current_img=current_img,
+                            animal=stats[0],
+                            confidence_score=stats[1],
+                            box_coordinates_normalised = stats[2],
+                            image_filename = stats[3]
+                            )
+    else:
+        return render_template("index.html", current_img=current_img)
 
-    return render_template("index.html", current_img=current_img)
-
+#
+# Navigate back to the list of subfolders
+#
 @app.route('/back', methods=["POST"])
 def back():
     # get list of all subdirectories in /results
     subfolders = os.listdir(os.path.join(app.static_folder, "results"))
     return render_template("index.html", subfolders=subfolders)
 
+#
 # Navigates to the previous image, keeping track of place in results folder
+#
 @app.route('/previous_img', methods=["POST"])
 def previous():
     global IMG_COUNTER
@@ -106,9 +173,28 @@ def previous():
         IMG_COUNTER = IMG_COUNTER - 1
     
     current_img = "/static/results/" + SUBFOLDER + "/" + IMAGES[IMG_COUNTER]
-    return render_template("index.html", current_img=current_img)
+    while(
+        not current_img.lower().endswith(('.png', '.jpg', '.jpeg', 'tiff', '.bmp', '.gif'))
+        and IMG_COUNTER != -1
+    ):
+        IMG_COUNTER -= 1
+        current_img = "/static/results/" + SUBFOLDER + "/" + IMAGES[IMG_COUNTER]
+    
+    stats = display_stats(IMAGES[IMG_COUNTER].split('.')[0], SUBFOLDER)
+    if stats is not None:
+        return render_template("index.html", 
+                            current_img=current_img,
+                            animal=stats[0],
+                            confidence_score=stats[1],
+                            box_coordinates_normalised = stats[2],
+                            image_filename = stats[3]
+                            )
+    else:
+        return render_template("index.html", current_img=current_img)
 
+#
 # Navigates to the next image, keeping track of place in results folder
+#
 @app.route('/next_img', methods=["POST"])
 def next():
     global IMG_COUNTER
@@ -117,12 +203,30 @@ def next():
         IMG_COUNTER = IMG_COUNTER + 1
     
     current_img = "/static/results/" + SUBFOLDER + "/" + IMAGES[IMG_COUNTER]
-    return render_template("index.html", current_img=current_img)
+    while(
+        not current_img.lower().endswith(('.png', '.jpg', '.jpeg', 'tiff', '.bmp', '.gif'))
+        and IMG_COUNTER != len(IMAGES) - 1
+        and IMG_COUNTER + 1 != len(IMAGES) -1
+    ):
+        IMG_COUNTER += 1
+        current_img = "/static/results/" + SUBFOLDER + "/" + IMAGES[IMG_COUNTER]
+    
+    
+    stats = display_stats(IMAGES[IMG_COUNTER].split('.')[0], SUBFOLDER)
+    if stats is not None:
+        return render_template("index.html", 
+                            current_img=current_img,
+                            animal=stats[0],
+                            confidence_score=stats[1],
+                            box_coordinates_normalised = stats[2],
+                            image_filename = stats[3]
+                            )
+    else:
+        return render_template("index.html", current_img=current_img)
 
-
-# """Interacting with the classifier module"""
-# # First check whether the target directory already exists - this will save previous results being overwritten accidently. Consider having a popup confirm intention with user
-# print(check_whether_target_directory_exists('/workspaces/animal-classifier-app/src'+'/'+'output'))
-# # Second request a single image to be classified; this also works for multiple images if you provide a source path for a directory
-# results = get_img_classification('src/models/best.pt', '/workspaces/animal-classifier-app/src/IMG_0290.jpg', '/workspaces/animal-classifier-app/src', 'output')
-# print(results)
+def display_stats(image_filename, subfolder):
+    filename = os.path.join(app.static_folder, "results/" + subfolder + "/" + image_filename + ".txt")
+    if (Path(filename).exists()):
+        with open(filename) as file:
+            lines = [line.rstrip() for line in file]
+        return lines
--- classifer.py
+++ classifer.py
@@ -2,7 +2,12 @@ from ultralytics import YOLO
 import os
 import logging
 import piexif
+from flask import Flask
 import shutil
+
+from PIL import Image
+
+app = Flask(__name__)
 
 def get_img_classification(vars_model, vars_source_image_path, vars_target_image_path, vars_target_directory):
     """
@@ -13,11 +18,11 @@ def get_img_classification(vars_model, vars_source_image_path, vars_target_image
     """
     vars_img_results = {} #initialise variable to return results
     model = YOLO(vars_model) #load model
-    results = model.predict(vars_source_image_path, conf=0.5, save=True, project=vars_target_image_path, name=vars_target_directory, exist_ok=True) #predict image
+    results = model.predict(vars_source_image_path, conf=0.5, exist_ok=True) #predict image
     
-    log_filename = 'classification.log' #log file name hardcoded
-    log_path = vars_target_image_path+'/'+vars_target_directory+'/'+log_filename # set location for log file
-    logging.basicConfig(level=logging.INFO, filename=log_path) # configure info level logging. This will be used resume sessions that prematurely die
+    # log_filename = 'classification.log' #log file name hardcoded
+    # log_path = vars_target_image_path+'/'+vars_target_directory+'/'+log_filename # set location for log file
+    # logging.basicConfig(level=logging.INFO, filename=log_path) # configure info level logging. This will be used resume sessions that prematurely die
     
     vars_img_results = {
                     'source_file_path': vars_source_image_path
@@ -26,25 +31,42 @@ def get_img_classification(vars_model, vars_source_image_path, vars_target_image
                     } #write header data
     box_list=[]
     for r in results:
-        try:
+        try:     
+            # Access the original image from the result
+            image_array = r.plot()
+                
+            # Convert the image array to PIL Image
+            image = Image.fromarray(image_array)
+                
+            # Generate a filename for the image
+            filename = os.path.basename(r.path)
+
+            # Check if there are any boxes detected
+            if len(r.boxes.data) == 0:
+                class_name = "unknown"
+            else:
+                class_id = int(r.boxes.data[0][-1])
+                class_name = model.names[class_id]
+
+            save_path = os.path.join(app.static_folder, "results", class_name)
+            os.makedirs(save_path, exist_ok=True)
+                
+            # Save the image using PIL
+            image.save(os.path.join(save_path, filename))
+            
             var = r.boxes.cls.item()
             box_list_item = {
-                            'animal': r.names[r.boxes.cls.item()]
-                            , 'confidence_score': r.boxes.conf.item()
-                            , 'box_coordinates_normalised': r.boxes.xywhn.numpy().tolist()[0]
-                            , 'image_path': r.path
-                            , 'image_filename': r.path.split('/')[-1]
+                            'animal': r.names[r.boxes.cls.item()],
+                            'confidence_score': r.boxes.conf.item(),
+                            'box_coordinates_normalised': r.boxes.xywhn.numpy().tolist()[0],
+                            'image_path': r.path,
+                            'image_filename': filename
                             } # write box payload data - can be multiple boxes/classifications
             box_list.append(box_list_item)
-            logging.info('CLASSIFICATION COMPLETE: '+r.path.split('/')[-1]) # log each file or batch of files that are processed by a single function call
-            add_metadata_tags_to_output_image_files(vars_target_image_path+'/'+vars_target_directory+'/'+r.path.split('/')[-1],r.names[r.boxes.cls.item()],r.boxes.conf.item())
-                # add metadata per reqs
-            write_file_to_subdirectory(r.path.split('/')[-1], r.names[r.boxes.cls.item()], vars_target_image_path, vars_target_directory)
-                # copy otuput image from output main directory to animal specific subdirectory
         except RuntimeError:
             continue
 
-    cleanup_image__output_directory(vars_target_image_path, vars_target_directory) # cleanup output directory
+    # cleanup_image__output_directory(vars_target_image_path, vars_target_directory) # cleanup output directory
 
     vars_img_results['boxes'] = box_list 
     
